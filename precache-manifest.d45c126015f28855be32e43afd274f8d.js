973b068107d0b28acc1e2d64d0d82fc07e5f3850
KaranManral <40225914+KaranManral@users.noreply.github.com>
2020-05-03 16:49:15 +0530

    Updates

---
+++ precache-manifest.d45c126015f28855be32e43afd274f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1afaf34ad0e4c9e87914da1b51cf0a",
+    "url": "/Shortly/index.html"
+  },
+  {
+    "revision": "b704a3aa5fa59f65e27d",
+    "url": "/Shortly/static/css/main.5a11e51e.chunk.css"
+  },
+  {
+    "revision": "fa2484271e38d60997ff",
+    "url": "/Shortly/static/js/2.a05330e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shortly/static/js/2.a05330e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b704a3aa5fa59f65e27d",
+    "url": "/Shortly/static/js/main.0998bd10.chunk.js"
+  },
+  {
+    "revision": "cb76d53baf7ca099df2b",
+    "url": "/Shortly/static/js/runtime-main.783df3c6.js"
+  },
+  {
+    "revision": "c5cd8cb18ee2790cbbd559103d778353",
+    "url": "/Shortly/static/media/va2.c5cd8cb1.jpg"
+  }
+]);
